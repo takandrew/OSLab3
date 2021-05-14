3b07ee0d62641440012ec3149bc37c38dca1dabf
takandrew <andreikaril@mail.ru>
2021-05-14 19:21:53 +0300

    Загрузил на Гитхаб

---
+++ OSLab3/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace OSLab3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MemoryTextBox = new System.Windows.Forms.TextBox();
+            this.PagesTextBox = new System.Windows.Forms.TextBox();
+            this.FIFOTextBox = new System.Windows.Forms.RichTextBox();
+            this.LRUTextBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(568, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Содержимое памяти";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(568, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Содержимое обращений";
+            // 
+            // MemoryTextBox
+            // 
+            this.MemoryTextBox.Location = new System.Drawing.Point(571, 106);
+            this.MemoryTextBox.Name = "MemoryTextBox";
+            this.MemoryTextBox.Size = new System.Drawing.Size(204, 22);
+            this.MemoryTextBox.TabIndex = 4;
+            this.MemoryTextBox.Text = "8 2 9 6 -1";
+            // 
+            // PagesTextBox
+            // 
+            this.PagesTextBox.Location = new System.Drawing.Point(571, 179);
+            this.PagesTextBox.Name = "PagesTextBox";
+            this.PagesTextBox.Size = new System.Drawing.Size(204, 22);
+            this.PagesTextBox.TabIndex = 5;
+            this.PagesTextBox.Text = "7 8 9 2 1 0 8 9 2 4 6 8 2 1 8 9";
+            // 
+            // FIFOTextBox
+            // 
+            this.FIFOTextBox.Location = new System.Drawing.Point(12, 43);
+            this.FIFOTextBox.Name = "FIFOTextBox";
+            this.FIFOTextBox.Size = new System.Drawing.Size(535, 403);
+            this.FIFOTextBox.TabIndex = 6;
+            this.FIFOTextBox.Text = "";
+            // 
+            // LRUTextBox
+            // 
+            this.LRUTextBox.Location = new System.Drawing.Point(805, 43);
+            this.LRUTextBox.Name = "LRUTextBox";
+            this.LRUTextBox.Size = new System.Drawing.Size(521, 403);
+            this.LRUTextBox.TabIndex = 7;
+            this.LRUTextBox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "FIFO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1035, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "LRU";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(599, 240);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(152, 62);
+            this.StartButton.TabIndex = 10;
+            this.StartButton.Text = "Запуск";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1338, 466);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LRUTextBox);
+            this.Controls.Add(this.FIFOTextBox);
+            this.Controls.Add(this.PagesTextBox);
+            this.Controls.Add(this.MemoryTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "Алгоритмы выталкивания памяти";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MemoryTextBox;
+        private System.Windows.Forms.TextBox PagesTextBox;
+        private System.Windows.Forms.RichTextBox FIFOTextBox;
+        private System.Windows.Forms.RichTextBox LRUTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ OSLab3/MainForm.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace OSLab3
+{
+    public partial class MainForm : Form
+    {
+        List<int> memoryList = new List<int>();
+        List<int> pagesList = new List<int>();
+        List<int> fifoList = new List<int>();
+        List<int> lruList = new List<int>();
+        int countOfInterruptions;
+        public MainForm()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+        }
+
+        private void GetMemory()
+        {
+            try
+            {
+                string dataString = MemoryTextBox.Text;
+                string[] splittedDataString = dataString.Split(' ');
+                int zero = 0;
+                int countSpaces = zero;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        countSpaces++;
+                    }
+                }
+                if (countSpaces == zero)
+                {
+                    MessageBox.Show("Исходные данные отсутствуют", "Внимание!");
+                    return;
+                }
+                string[] withoutSpacesDataString = new string[countSpaces];
+                int j = 0;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        withoutSpacesDataString[j] = splittedDataString[i];
+                        j++;
+                    }
+                }
+                for (int i = 0; i < withoutSpacesDataString.Length; i++)
+                {
+                    memoryList.Add(Convert.ToInt32(withoutSpacesDataString[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка ввода");
+            }
+        }
+
+        private void GetPages()
+        {
+            try
+            {
+                string dataString = PagesTextBox.Text;
+                string[] splittedDataString = dataString.Split(' ');
+                int zero = 0;
+                int countSpaces = zero;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        countSpaces++;
+                    }
+                }
+                if (countSpaces == zero)
+                {
+                    MessageBox.Show("Исходные данные отсутствуют", "Внимание!");
+                    return;
+                }
+                string[] withoutSpacesDataString = new string[countSpaces];
+                int j = 0;
+                for (int i = 0; i < splittedDataString.Length; i++)
+                {
+                    if (splittedDataString[i] != "")
+                    {
+                        withoutSpacesDataString[j] = splittedDataString[i];
+                        j++;
+                    }
+                }
+                for (int i = 0; i < withoutSpacesDataString.Length; i++)
+                {
+                    pagesList.Add(Convert.ToInt32(withoutSpacesDataString[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка ввода");
+            }
+        }
+
+        private void FIFO()
+        {
+            for (int m = 0; m < memoryList.Count; m++)
+                fifoList.Add(memoryList[m]);
+            countOfInterruptions = 0;
+            for (int k = 0; k < pagesList.Count; k++) 
+            {
+                string interruptionTag = "";
+                if (!fifoList.Contains(pagesList[k])) 
+                {
+                    countOfInterruptions++;
+                    if (fifoList[fifoList.Count - 1] != -1)
+                    {
+                        for (int i = 0; i < fifoList.Count - 1; i++)
+                        {
+                            fifoList[i] = fifoList[i + 1];
+                        }
+                        fifoList[fifoList.Count - 1] = pagesList[k];
+                    }
+                    else
+                    {
+                        fifoList[fifoList.Count - 1] = pagesList[k]; 
+                    }
+                    interruptionTag = " [Прерывание]";
+                }
+                if (k >= 9)
+                    FIFOTextBox.Text += "Обращение№ " + (k + 1) + " Страница: " + pagesList[k]
+                    + " Память: " + string.Join(",", fifoList) + interruptionTag + "\n";
+                else
+                    FIFOTextBox.Text += "Обращение№ " + (k + 1) + "   Страница: " + pagesList[k]
+                    + " Память: " + string.Join(",", fifoList) + interruptionTag + "\n";
+            }
+            FIFOTextBox.Text += " Число прерываний: " + countOfInterruptions;
+        }
+
+        private void LRU()
+        {
+            countOfInterruptions = 0;
+            for (int m = 0; m < memoryList.Count; m++)
+                lruList.Add(memoryList[m]);
+            for (int k = 0; k < pagesList.Count; k++)
+            {
+                string interruptionTag = "     ";
+
+                if (!lruList.Contains(pagesList[k])) 
+                {
+                    countOfInterruptions++; 
+                    if (lruList[lruList.Count - 1] != -1) 
+                    {
+                        for (int i = 0; i < lruList.Count - 1; i++)
+                        {
+                            lruList[i] = lruList[i + 1]; 
+                        }
+                        lruList[lruList.Count - 1] = pagesList[k]; 
+
+                    }
+                    else
+                        lruList[lruList.Count - 1] = pagesList[k]; 
+                    interruptionTag = " [Прерывание]"; 
+                }
+                else 
+                {
+                    int n;
+                    if (lruList.Contains(-1))
+                        n = 2; 
+                    else 
+                        n = 1; 
+                    int temp = lruList.IndexOf(pagesList[k]);
+                    int swaping = lruList[temp];
+                    for (int i = temp; i < lruList.Count - n; i++) 
+                    {
+                        lruList[i] = lruList[i + 1];
+                    }
+                    lruList[lruList.Count - n] = swaping;
+                }
+                if (k >= 9)
+                    LRUTextBox.Text += "Обращение№ " + (k + 1) + " Страница: " + pagesList[k]
+                    + " Память: " + string.Join(",", lruList) + interruptionTag + "\n";
+                else
+                    LRUTextBox.Text += "Обращение№ " + (k + 1) + "   Страница: " + pagesList[k]
+                    + " Память: " + string.Join(",", lruList) + interruptionTag + "\n";
+            }
+            LRUTextBox.Text += " Число прерываний: " + countOfInterruptions;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            memoryList.Clear();
+            pagesList.Clear();
+            fifoList.Clear();
+            lruList.Clear();
+            FIFOTextBox.Clear();
+            LRUTextBox.Clear();
+            GetMemory();
+            GetPages();
+            FIFO();
+            LRU();
+        }
+    }
+}
